30d568138a27496d8c5a84471bf05580823d5a6d
Jaggy <jaggy@humans.ph>
2023-02-12 23:09:34 +0800

    Set up the base config

--- src/FilamentS3MultipartUploadServiceProvider.php
+++ src/FilamentS3MultipartUploadServiceProvider.php
@@ -3,7 +3,15 @@
 namespace CloudMazing\FilamentS3MultipartUpload;
 
 use Filament\PluginServiceProvider;
+use Spatie\LaravelPackageTools\Package;
 
 class FilamentS3MultipartUploadServiceProvider extends PluginServiceProvider
 {
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('filament-s3-multipart-upload')
+            ->hasConfigFile()
+        ;
+    }
 }
