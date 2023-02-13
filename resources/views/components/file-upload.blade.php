4ecba54e8c4de43abe87e82870d439646d981f75
Jaggy <jaggy@humans.ph>
2023-02-13 09:31:37 +0800

    wip

--- resources/views/components/file-upload.blade.php
+++ resources/views/components/file-upload.blade.php
@@ -1,12 +1,23 @@
-<div class="UppyDragDrop">
+<div>
+    {{ $property }}
+    <div class="input--uppy">
+    </div>
 </div>
 
 <link href="https://releases.transloadit.com/uppy/v2.3.0/uppy.min.css" rel="stylesheet">
 <script src="https://releases.transloadit.com/uppy/v2.3.0/uppy.min.js"></script>
 <script>
     document.addEventListener('alpine:init', () => {
-        var uppy = new Uppy.Core()
-        uppy.use(Uppy.DragDrop, { target: '.UppyDragDrop' })
-        uppy.use(Uppy.AwsS3Multipart)
+        const { Core, DragDrop, AwsS3Multipart } = Uppy
+
+        const uppy = new Core()
+
+        uppy.use(DragDrop, {
+            target: '.input--uppy' ,
+        })
+
+        uppy.use(AwsS3Multipart, {
+            limit: 1,
+        })
     })
 </script>
--- src/Components/FileUpload.php
+++ src/Components/FileUpload.php
@@ -7,4 +7,6 @@ use Filament\Forms\Components\Field;
 class FileUpload extends Field
 {
     protected string $view = 'filament-s3-multipart-upload::components.file-upload';
+
+    public string $property = 'hello';
 }
