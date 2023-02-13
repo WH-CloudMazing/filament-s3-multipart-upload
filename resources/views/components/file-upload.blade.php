2e687ca95f9207540ec545ace1ed39f2e31df3e5
Jaggy <jaggy@humans.ph>
2023-02-13 09:27:23 +0800

    Use a CDN for now

--- resources/views/components/file-upload.blade.php
+++ resources/views/components/file-upload.blade.php
@@ -1,1 +1,12 @@
-I am a component
+<div class="UppyDragDrop">
+</div>
+
+<link href="https://releases.transloadit.com/uppy/v2.3.0/uppy.min.css" rel="stylesheet">
+<script src="https://releases.transloadit.com/uppy/v2.3.0/uppy.min.js"></script>
+<script>
+    document.addEventListener('alpine:init', () => {
+        var uppy = new Uppy.Core()
+        uppy.use(Uppy.DragDrop, { target: '.UppyDragDrop' })
+        uppy.use(Uppy.AwsS3Multipart)
+    })
+</script>
