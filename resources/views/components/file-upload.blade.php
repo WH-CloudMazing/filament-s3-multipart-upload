42dbbb089f6cdb54d3b13f4b5137133dd3eb0bff
Jaggy <jaggy@humans.ph>
2023-02-14 16:30:14 +0800

    Use the filament form wrapper

--- resources/views/components/file-upload.blade.php
+++ resources/views/components/file-upload.blade.php
@@ -1,41 +1,52 @@
-<div x-data="uppy" class="uppy">
-    <input
-        type="hidden"
-        name="{{ $getName() }}"
-        x-model="state"
+<x-forms::field-wrapper
+    :id="$getId()"
+    :label="$getLabel()"
+    :label-sr-only="$isLabelHidden()"
+    :helper-text="$getHelperText()"
+    :hint="$getHint()"
+    :hint-icon="$getHintIcon()"
+    :required="$isRequired()"
+    :state-path="$getStatePath()"
+>
+    <div x-data="uppy" class="uppy">
+        <input
+            type="hidden"
+            name="{{ $getName() }}"
+            x-model="state"
         {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
-    >
+        >
 
-    <div class="uppy__input">
-    </div>
+        <div class="uppy__input">
+        </div>
 
-    <div class="uppy__progress-bar">
-    </div>
+        <div class="uppy__progress-bar">
+        </div>
 
-    <div class="uppy__files mt-2">
-        <template x-for="file in uploadedFiles" :key="file.id">
-            <div class="uppy__file file py-2 px-4 text-sm bg-white">
-                <div>
-                    <span class="file__name font-bold text-sm" x-text="file.name"></span>
+        <div class="uppy__files mt-2">
+            <template x-for="file in uploadedFiles" :key="file.id">
+                <div class="uppy__file file py-2 px-4 text-sm bg-white">
+                    <div>
+                        <span class="file__name font-bold text-sm" x-text="file.name"></span>
+                    </div>
+
+                    <div class="file__meta space-x-2 text-xs text-neutral-700">
+                        <span class="file__size" x-text="bytesToSize(file.size)"></span>
+                        <span class="file__type" x-text="file.type"></span>
+                    </div>
                 </div>
+            </template>
+        </div>
 
-                <div class="file__meta space-x-2 text-xs text-neutral-700">
-                    <span class="file__size" x-text="bytesToSize(file.size)"></span>
-                    <span class="file__type" x-text="file.type"></span>
-                </div>
-            </div>
-        </template>
+        @unless($hasAwsConfigured())
+            <p>No AWS S3 configuration found.</p>
+        @endunless
     </div>
-
-    @unless($hasAwsConfigured())
-        <p>No AWS S3 configuration found.</p>
-    @endunless
-</div>
+</x-forms::field-wrapper>
 
 <link href="https://releases.transloadit.com/uppy/v2.3.0/uppy.min.css" rel="stylesheet">
 
 <script type="module">
-    import { Uppy, AwsS3Multipart, DragDrop, StatusBar } from "https://releases.transloadit.com/uppy/v3.4.0/uppy.min.mjs"
+    import {Uppy, AwsS3Multipart, DragDrop, StatusBar} from "https://releases.transloadit.com/uppy/v3.4.0/uppy.min.mjs"
 
     document.addEventListener('alpine:init', () => {
         Alpine.data('uppy', () => ({
@@ -43,7 +54,7 @@
             state: null,
             uploadedFiles: [],
 
-            bytesToSize (bytes) {
+            bytesToSize(bytes) {
                 const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
                 if (bytes === 0) return "n/a"
                 const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
@@ -51,7 +62,7 @@
                 return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`
             },
 
-            init () {
+            init() {
                 this.state = this.$wire.entangle('{{ $getStatePath() }}')
 
                 this.uppy = new Uppy({
