94ae330767b02b5a28d50f6a267f72c2292763d1
Sami Mazouz <sychocouldy@gmail.com>
2024-04-01 11:52:04 +0100

    chore: filament v3 change

--- resources/views/components/file-upload.blade.php
+++ resources/views/components/file-upload.blade.php
@@ -86,16 +86,7 @@
 
 <x-dynamic-component
     :component="$getFieldWrapperView()"
-    :id="$getId()"
-    :label="$getLabel()"
-    :label-sr-only="$isLabelHidden()"
-    :helper-text="$getHelperText()"
-    :hint="$getHint()"
-    :hint-action="$getHintAction()"
-    :hint-color="$getHintColor()"
-    :hint-icon="$getHintIcon()"
-    :required="$isRequired()"
-    :state-path="$getStatePath()"
+    :field="$field"
 >
     <div x-data="useUppy($wire)" class="uppy">
         <input
